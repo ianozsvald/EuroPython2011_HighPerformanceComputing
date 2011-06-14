edfc049768c209e1c3ca2ffd7e98c30965302ccc
Ian Ozsvald <ian@ianozsvald.com>
2011-06-14 20:24:24 +0100

    first

---
+++ mandelbrot/python/pure_python_2.py
@@ -0,0 +1,72 @@
+import sys
+import datetime
+# area of space to investigate
+x1, x2, y1, y2 = -2.13, 0.77, -1.3, 1.3
+
+# Original code, prints progress (because it is slow)
+# Uses complex datatype
+# Doesn't dereference on each iteration, goes faster!
+
+def calculate_z_serial_purepython(q, maxiter, z):
+    """Pure python with complex datatype, iterating over list of q and z"""
+    output = [0] * len(q)
+    for i in range(len(q)):
+        zi = z[i]
+        qi = q[i]
+        if i % 1000 == 0:
+            # print out some progress info since it is so slow...
+            print "%0.2f%% complete" % (1.0/len(q) * i * 100)
+        for iteration in range(maxiter):
+            #z[i] = z[i]*z[i] + q[i]
+            zi = zi * zi + qi
+            #if abs(z[i]) > 2.0:
+            if abs(zi) > 2.0:
+                output[i] = iteration
+                break
+    return output
+
+def calc_pure_python(show_output):
+    """Create z and q arrays without np.arange"""
+    x_step = (float(x2 - x1) / float(w)) * 2
+    y = y2
+    y_step = (float(y1 - y2) / float(h)) * 2
+    q = []
+    while y > y1:
+        x = x1
+        while x < x2:
+            q.append(complex(x,y))
+            x += x_step
+        y += y_step
+    z = [0+0j] * len(q)
+    print "Total elements:", len(z)
+    start_time = datetime.datetime.now()
+    output = calculate_z_serial_purepython(q, maxiter, z)
+    end_time = datetime.datetime.now()
+    secs = end_time - start_time
+    print "Main took", secs
+
+    validation_sum = sum(output)
+    print "Total sum of elements (for validation):", validation_sum
+
+    if show_output: 
+        try:
+            import Image
+            import numpy as nm
+            output = nm.array(output)
+            output = (output + (256*output) + (256**2)*output) * 8
+            im = Image.new("RGB", (w/2, h/2))
+            im.fromstring(output.tostring(), "raw", "RGBX", 0, -1)
+            im.show()
+        except ImportError as err:
+            # Bail gracefully if we're using PyPy
+            print "Couldn't import Image or numpy:", str(err)
+
+if __name__ == "__main__":
+    # get width, height and max iterations from cmd line
+    # 'python mandelbrot_pypy.py 100 300'
+    w = int(sys.argv[1]) # e.g. 100
+    h = int(sys.argv[1]) # e.g. 100
+    maxiter = int(sys.argv[2]) # e.g. 300
+    
+    # we can show_output for Python, not for PyPy
+    calc_pure_python(True)
