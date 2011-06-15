3be3e8b7056eed38fe2c10fc878c81aa57f956f8
Ian Ozsvald <ian@ianozsvald.com>
2011-06-15 21:29:56 +0100

    added comments

--- mandelbrot/python/pure_python.py
+++ mandelbrot/python/pure_python.py
@@ -3,8 +3,11 @@ import datetime
 # area of space to investigate
 x1, x2, y1, y2 = -2.13, 0.77, -1.3, 1.3
 
+# Original code, prints progress (because it is slow)
+# Uses complex datatype
 
 def calculate_z_serial_purepython(q, maxiter, z):
+    """Pure python with complex datatype, iterating over list of q and z"""
     output = [0] * len(q)
     for i in range(len(q)):
         if i % 1000 == 0:
@@ -13,13 +16,12 @@ def calculate_z_serial_purepython(q, maxiter, z):
         for iteration in range(maxiter):
             z[i] = z[i]*z[i] + q[i]
             if abs(z[i]) > 2.0:
-                q[i] = 0+0j
-                z[i] = 0+0j
                 output[i] = iteration
                 break
     return output
 
 def calc_pure_python(show_output):
+    """Create z and q arrays without np.arange"""
     x_step = (float(x2 - x1) / float(w)) * 2
     y = y2
     y_step = (float(y1 - y2) / float(h)) * 2
@@ -27,7 +29,6 @@ def calc_pure_python(show_output):
     while y > y1:
         x = x1
         while x < x2:
-            # work here
             q.append(complex(x,y))
             x += x_step
         y += y_step
@@ -43,13 +44,17 @@ def calc_pure_python(show_output):
     print "Total sum of elements (for validation):", validation_sum
 
     if show_output: 
-        import Image
-        import numpy as nm
-        output = nm.array(output)
-        output = (output + (256*output) + (256**2)*output) * 8
-        im = Image.new("RGB", (w/2, h/2))
-        im.fromstring(output.tostring(), "raw", "RGBX", 0, -1)
-        im.show()
+        try:
+            import Image
+            import numpy as nm
+            output = nm.array(output)
+            output = (output + (256*output) + (256**2)*output) * 8
+            im = Image.new("RGB", (w/2, h/2))
+            im.fromstring(output.tostring(), "raw", "RGBX", 0, -1)
+            im.show()
+        except ImportError as err:
+            # Bail gracefully if we're using PyPy
+            print "Couldn't import Image or numpy:", str(err)
 
 if __name__ == "__main__":
     # get width, height and max iterations from cmd line
