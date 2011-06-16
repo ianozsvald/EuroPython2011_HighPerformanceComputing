98e3efb71c097fedec6ea22e282c6cc53da26a30
Ian Ozsvald <ian@ianozsvald.com>
2011-06-16 21:32:17 +0100

    slightly faster

--- mandelbrot/numpy_vector/numpy_vector_2.py
+++ mandelbrot/numpy_vector/numpy_vector_2.py
@@ -12,14 +12,16 @@ x1, x2, y1, y2 = -2.13, 0.77, -1.3, 1.3
 
 def calculate_z_numpy(q_full, maxiter, z_full):
     output = np.resize(np.array(0,), q_full.shape)
-    #STEP_SIZE = len(q_full) # 54s
-    STEP_SIZE = 90000 # 52
+    #STEP_SIZE = len(q_full) # 54s for 250,000
+    #STEP_SIZE = 90000 # 52
     #STEP_SIZE = 50000 # 45s
     #STEP_SIZE = 45000 # 45s
     #STEP_SIZE = 20000 # 42s
     #STEP_SIZE = 10000 # 43s
     #STEP_SIZE = 5000 # 45s
     #STEP_SIZE = 1000 # 1min02
+    STEP_SIZE = 100
+    print "STEP_SIZE", STEP_SIZE
     for step in range(0, len(q_full), STEP_SIZE):
         z = z_full[step:step+STEP_SIZE]
         q = q_full[step:step+STEP_SIZE]
@@ -30,7 +32,7 @@ def calculate_z_numpy(q_full, maxiter, z_full):
             q = np.where(done,0+0j, q)
             z = np.where(done,0+0j, z)
             output[step:step+STEP_SIZE] = np.where(done, iteration, output[step:step+STEP_SIZE])
-            nbr_just_done = np.sum(np.where(done, 1, 0))
+            #nbr_just_done = np.sum(np.where(done, 1, 0))
             #nbr_done += nbr_just_done
             #if (output[step:step+STEP_SIZE] > 0).all():
             #if nbr_done == STEP_SIZE:
