da54c9a391b787cfdada9603bba9ac86110a76b6
Ian Ozsvald <ian@ianozsvald.com>
2011-06-16 21:31:22 +0100

    moved z back to calculate_z

--- mandelbrot/cython_numpy_loop/calculate_z.pyx
+++ mandelbrot/cython_numpy_loop/calculate_z.pyx
@@ -10,30 +10,33 @@ Attribution: Didrik Pinte (Enthought) for first version
 from numpy import empty, zeros
 cimport numpy as np
 
-cdef int mandelbrot_escape(double complex c, int n):
+cdef int mandelbrot_escape(double complex q, int maxiter, double complex z):
     """ Mandelbrot set escape time algorithm in real and complex components
     """
-    cdef double complex z
     cdef int i
-    z = 0
-    for i in range(n):
-        z = z*z + c
+    for i in range(maxiter):
+        z = z*z + q
         if z.real*z.real + z.imag*z.imag > 4.0:  
             break
     else:
         i = 0
     return i
 
-def calculate_z(np.ndarray[double, ndim=1] xs, np.ndarray[double, ndim=1] ys, int n):
+def calculate_z(np.ndarray[double, ndim=1] xs, np.ndarray[double, ndim=1] ys, int maxiter):
     """ Generate a mandelbrot set """
     cdef unsigned int i,j
     cdef unsigned int N = len(xs)
     cdef unsigned int M = len(ys)
+    cdef double complex q
     cdef double complex z
+    z = 0+0j
     
     cdef np.ndarray[int, ndim=2] d = empty(dtype='i', shape=(M, N))
     for j in range(M):
         for i in range(N):
-            z = xs[i] + ys[j]*1j
-            d[j,i] = mandelbrot_escape(z, n)
+            # create q without intermediate object (faster)
+            q = xs[i] + ys[j]*1j
+            # create a temporary complex (consistently a bit slower)
+            #q = complex(xs[i], ys[j])
+            d[j,i] = mandelbrot_escape(q, maxiter, z)
     return d
