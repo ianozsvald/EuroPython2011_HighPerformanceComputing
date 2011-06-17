ed8d7e856248c8aa5373b580692111ca7a40b941
Ian Ozsvald <ian@ianozsvald.com>
2011-06-17 10:38:44 +0100

    minor

--- mandelbrot/numpy_loop/calculate_z.py
+++ mandelbrot/numpy_loop/calculate_z.py
@@ -1,28 +1,26 @@
 
 import numpy as np
 
-def mandelbrot_escape(c, n):
+def mandelbrot_escape(q, n, z):
     """ Mandelbrot set escape time algorithm in real and complex components
     """
-    z = 0 # DP : Ian does initialise it to zero, we do initialise it to c
     for i in range(n):
-        z = z*z + c
-        if z.real*z.real + z.imag*z.imag > 4.0:  # DP :  was >=
-        #if abs(z) > 2.0:  # DP :  was >=
+        z = z*z + q
+        if z.real*z.real + z.imag*z.imag > 4.0:
            break
     else:
-        i = 0 # DP : was returning -1 when not enough iterations ...
+        i = 0 
     return i
 
-def generate_mandelbrot(xs, ys, n):
+def calculate_z(xs, ys, maxiter):
     """ Generate a mandelbrot set """
     N = len(xs)
     M = len(ys)
+    z = 0+0j
     
-    #np.ndarray[int, ndim=2] d = empty(dtype='i', shape=(M, N))
     d = np.zeros((M, N)).astype(np.int)
     for j in range(M):
         for i in range(N):
-            z = xs[i] + ys[j]*1j
-            d[j,i] = mandelbrot_escape(z, n)
+            q = xs[i] + ys[j]*1j
+            d[j,i] = mandelbrot_escape(q, maxiter, z)
     return d
