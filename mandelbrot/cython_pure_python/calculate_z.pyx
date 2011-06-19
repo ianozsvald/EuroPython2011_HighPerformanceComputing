3fcd1d6cc43a8c1439cb4e223e1270bc8bd803b2
Ian Ozsvald <ian@ianozsvald.com>
2011-06-19 01:38:49 +0100

    cleaner

--- mandelbrot/cython_pure_python/calculate_z.pyx
+++ mandelbrot/cython_pure_python/calculate_z.pyx
@@ -1,26 +1,18 @@
-def calculate_z(list q, int maxiter, list z):
-    cdef unsigned int i
-    cdef int iteration
-    cdef list output
-    cdef double zx, zy, qx, qy, zx_new, zy_new
+# based on calculate_z_serial_purepython
+def calculate_z(q, maxiter, z):
+#def calculate_z(list q, int maxiter, list z):
+    #cdef unsigned int i
+    #cdef int iteration
+    #cdef complex zi, qi
+    #cdef list output
 
     output = [0] * len(q)
     for i in range(len(q)):
-        zx = z[i].real # need to extract items using dot notation
-        zy = z[i].imag
-        qx = q[i].real
-        qy = q[i].imag
-
+        zi = z[i]
+        qi = q[i]
         for iteration in range(maxiter):
-            zx_new = (zx * zx - zy * zy) + qx
-            zy_new = (zx * zy + zy * zx) + qy
-            # must assign after else we're using the new zx/zy in the fla
-            zx = zx_new
-            zy = zy_new
-            # note - math.sqrt makes this almost twice as slow!
-            #if math.sqrt(zx*zx + zy*zy) > 2.0:
-            if (zx*zx + zy*zy) > 4.0:
+            zi = zi * zi + qi
+            if abs(zi) > 2.0:
                 output[i] = iteration
                 break
     return output
-
